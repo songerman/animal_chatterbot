613ec58701a82eab58641bb4d9daccd87d901e71
gbegbwltgb <55059000+gbegbwltgb@users.noreply.github.com>
2019-12-09 15:03:22 +0500

    first commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,49 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help")) {
+                sendMsg(message, "Чем я могу помочь?");
+            } else {
+                sendMsg(message, "Я всего лишь бот...........");
+            }
+        }
+    }
+
+    public synchronized void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "guess_animal_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1054921717:AAFAvaUJvgCcli-iT1xvCSHUHkTTPOqkDxc";
+    }
+}
