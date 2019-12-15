1cd192a23e2fd8bc5875436ce73051c772cd0624
gbegbwltgb <55059000+gbegbwltgb@users.noreply.github.com>
2019-12-15 19:49:32 +0500

    Update Program.java

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,9 +8,9 @@ import java.lang.reflect.Method;
 public class Program {
 
     public static void main(String[] args) {
-        System.getProperties().put( "proxySet", "true" );
-        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-        System.getProperties().put( "socksProxyPort", "9050" );
+        //System.getProperties().put( "proxySet", "true" );
+        //System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+        //System.getProperties().put( "socksProxyPort", "9050" );
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
